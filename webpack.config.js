4a11df353235c2ce162f9cb2046371544271ea11
Wei Lin <anzhihun@gmail.com>
2016-07-14 19:05:20 +0800

    format webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
 module.exports = {
-  entry: {
-    f3Earth: './src/earth.js',
-    mouseWheelZoomInteraction:'./src/interaction/mouseWheelZoomInteraction.js',
-    dragInteraction:'./src/interaction/dragInteraction.js',
-    doubleClickZoomInteraction: './src/interaction/doubleClickZoomInteraction.js'
-  },
-  output: {
-    libraryTarget: 'umd',
-    filename: 'dist/[name].js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          compact: false
-        }
-      }
-    ],
-    noParse: [/proj4/]
-  },
+    entry: {
+        f3Earth: './src/earth.js',
+        mouseWheelZoomInteraction: './src/interaction/mouseWheelZoomInteraction.js',
+        dragInteraction: './src/interaction/dragInteraction.js',
+        doubleClickZoomInteraction: './src/interaction/doubleClickZoomInteraction.js'
+    },
+    output: {
+        libraryTarget: 'umd',
+        filename: 'dist/[name].js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    compact: false
+                }
+            }
+        ],
+        noParse: [/proj4/]
+    }
 };
