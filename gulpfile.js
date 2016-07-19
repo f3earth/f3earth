64e18c071798ad1609fd182f2fc4ea2cf1276dbe
张羽 <michael.zy@163.com>
2016-07-19 23:01:33 +0800

    add the zoomControl and update the attributionControl;

--- gulpfile.js
+++ gulpfile.js
@@ -8,15 +8,17 @@ gulp.task('lint', () =>
     // Also, Be sure to return the stream from the task;
     // Otherwise, the task may end before the stream has finished.
     gulp.src(['gulpfile.js', 'src/**/*.js', 'examples/**/*.js'])
-        // eslint() attaches the lint output to the "eslint" property
-        // of the file object so it can be used by other modules.
-        .pipe(eslint({ useEslintrc: true }))
-        // eslint.format() outputs the lint results to the console.
-        // Alternatively use eslint.formatEach() (see Docs).
-        .pipe(eslint.format())
-        // To have the process exit with an error code (1) on
-        // lint error, return the stream and pipe to failAfterError last.
-        .pipe(eslint.failAfterError())
+    // eslint() attaches the lint output to the "eslint" property
+    // of the file object so it can be used by other modules.
+    .pipe(eslint({
+        useEslintrc: true
+    }))
+    // eslint.format() outputs the lint results to the console.
+    // Alternatively use eslint.formatEach() (see Docs).
+    .pipe(eslint.format())
+    // To have the process exit with an error code (1) on
+    // lint error, return the stream and pipe to failAfterError last.
+    .pipe(eslint.failAfterError())
 );
 
 gulp.task('webpack', () => {
@@ -27,23 +29,22 @@ gulp.task('webpack', () => {
             dragInteraction: './src/interaction/dragInteraction.js',
             doubleClickZoomInteraction: './src/interaction/doubleClickZoomInteraction.js',
             // control: './src/control/control.js'
-            attributionControl: './src/control/attributionControl.js'
+            attributionControl: './src/control/attributionControl.js',
+            zoomControl: './src/control/zoomControl.js'
         },
         output: {
             libraryTarget: 'umd',
             filename: 'dist/[name].js'
         },
         module: {
-            loaders: [
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    loader: 'babel',
-                    query: {
-                        compact: false
-                    }
+            loaders: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    compact: false
                 }
-            ],
+            }],
             noParse: [/proj4/]
         }
     };
--- webpack.config.js
+++ webpack.config.js
@@ -5,23 +5,22 @@ module.exports = {
         dragInteraction: './src/interaction/dragInteraction.js',
         doubleClickZoomInteraction: './src/interaction/doubleClickZoomInteraction.js',
         // control: './src/control/control.js'
-        attributionControl: './src/control/attributionControl.js'
+        attributionControl: './src/control/attributionControl.js',
+        zoomControl: './src/control/zoomControl.js'
     },
     output: {
         libraryTarget: 'umd',
         filename: 'dist/[name].js'
     },
     module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel',
-                query: {
-                    compact: false
-                }
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                compact: false
             }
-        ],
+        }],
         noParse: [/proj4/]
     }
 };
