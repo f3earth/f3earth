ac4473afb317f960a35af7a054752c3e485b42ef
zhangwenjin <253254412@qq.com>
2016-07-22 15:33:12 +0800

    Add PointLayer

--- src/shader/layerShader.js
+++ src/shader/layerShader.js
@@ -49,4 +49,29 @@ export class LayerShader {
           } 
         `;
     }
+    static get pointVertexSource() {
+        return `
+          attribute vec3 aVertexPosition;
+
+          uniform mat4 uMVMatrix;
+          uniform mat4 uPMatrix;
+
+          void main() {
+            gl_PointSize = 10.0;
+            gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);
+          } 
+        `;
+    }
+
+    static get pointFragmentSource() {
+        return `
+          precision mediump float;
+
+          uniform vec4 uColor;
+
+          void main() {
+            gl_FragColor = uColor;
+          } 
+        `;
+    }
 }
